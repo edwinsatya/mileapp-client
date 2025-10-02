6fc5fb5e0e218e7dd026961fa846345e62fa7e40
edwin satya <edwinsatyayudistira@gmail.com>
2025-10-02 17:51:25 +0700

    pre start setup something and add tailwind css

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    test main page
+  </div>
+</template>
---
+++ app/pages/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mx-auto p-4 bg-blue-100">
+    <h1 class="text-2xl font-bold text-blue-700">Hello Tailwind!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
